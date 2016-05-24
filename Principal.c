1957ecadd67e590a635379cb9db562982e722aca
Jefferson Santos <jefferson.npng1@gmail.com>
2016-05-23 21:40:56 -0300

    Update Principal.c

--- Principal.c
+++ Principal.c
@@ -15,6 +15,27 @@ void inverter_principal(Lista *l) {
 	
 }
 
+void esta_ordem_principal(Lista *l){
+	int n_ma = 0, n_me = 0;
+	if ( tamanho(l) > 2) {
+	
+	for ( i = 0; i<tamanho(l) - 1 ;i++){
+		int x = ler_pos(l,i);
+		y = ler_pos(l,i+1);
+		
+		if (x < y)
+		n_me++;
+		if(x > y)
+		n_ma++;
+		
+		if (n_ma > 0 && n_ma > 0)
+		return 0;
+
+	}
+}
+	return 1;	
+}
+
 int main(int argc, char *argv[]) {
 	Lista * l = criar();
 	inserir_ultimo(l, 2);
@@ -31,6 +52,7 @@ int main(int argc, char *argv[]) {
 	inserir_primeiro(l, 3);
 	imprimir(l);
 	inserir_ultimo(l, 8);
+	printf("Ordenada: %s", esta_ordem_principal(l)? "Sim":"Nao");
 	imprimir(l);
 	destruir(l);
 	system("pause");
