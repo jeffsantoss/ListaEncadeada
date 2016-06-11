fd05b5251c69b7bdd8e5ab5ae410fa02798d1406
Jefferson Santos <jefferson.npng1@gmail.com>
2016-06-11 11:59:17 -0300

    Update Principal.c

--- Principal.c
+++ Principal.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Lista.h"
-#include "Lista.c"
+#include "Lista.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void inverter_principal(Lista *l) {
-	int i;
-	for (i=0; i < tamanho(l) / 2 ; i++){
+	int i,tmp;
+	
+	for (i=0; i < tamanho(l) / 2 ; i++)
+	{
 		tmp = ler_pos(l,i);
 		escrever_pos(l,i,ler_pos(l,tamanho(l) - i - 1));
 		escrever_pos(l,tamanho(l)-i-1, tmp);
@@ -15,13 +17,14 @@ void inverter_principal(Lista *l) {
 	
 }
 
-void esta_ordem_principal(Lista *l){
+int esta_ordem_principal(Lista *l){
 	int n_ma = 0, n_me = 0;
+	int i;
 	if ( tamanho(l) > 2) {
 	
 	for ( i = 0; i<tamanho(l) - 1 ;i++){
 		int x = ler_pos(l,i);
-		y = ler_pos(l,i+1);
+		int y = ler_pos(l,i+1);
 		
 		if (x < y)
 		n_me++;
@@ -41,13 +44,12 @@ int main(int argc, char *argv[]) {
 	inserir_ultimo(l, 2);
 	imprimir(l);
 	inserir_ultimo(l, 5);
-	system("pause");
 	imprimir(l);
 	inserir_ultimo(l, 7);
 	imprimir(l);
 	inserir_ultimo(l, 9);
 	imprimir(l);
-	inserir_pos(l, 2, 4);
+	inserir_pos(l, 0, 4);
 	imprimir(l);
 	inserir_primeiro(l, 3);
 	imprimir(l);
@@ -55,5 +57,6 @@ int main(int argc, char *argv[]) {
 	printf("Ordenada: %s", esta_ordem_principal(l)? "Sim":"Nao");
 	imprimir(l);
 	destruir(l);
+	
 	system("pause");
 }
